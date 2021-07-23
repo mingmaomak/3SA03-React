7c46ab9ca63adf49107c924a38338cdf119f90d5
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2021-07-23 14:41:35 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -23,11 +23,13 @@ import './App.css';
 //   );
 // }
 import CharacterCard from './CharacterCard'; 
+const word = "Hello"; 
 function App() { 
   return ( 
     <div> 
-      <CharacterCard value="h"/> 
-      <CharacterCard value="i"/> 
+      { 
+         Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>) 
+      } 
     </div> 
   ); 
 }
