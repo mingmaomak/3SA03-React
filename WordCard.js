99d638ff1390b8a1b8d1a02eda842504f86daf28
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2021-07-23 18:26:35 +0700

    Add files via upload

--- WordCard.js
+++ WordCard.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import _ from 'lodash'; 
 import CharacterCard from './CharacterCard';
 
 const activationHandler = c => { console.log(`${c} has been activated.`) } 
 
-
-
 export default function WordCard(props){ 
     return ( 
       <div> 
-        
         { 
         Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler={activationHandler}/> ) 
-        
         } 
       </div> 
     ); 
 }
 
+const prepareStateFromWord = (given_word) => { 
+    let word = given_word.toUpperCase() 
+    let chars = _.shuffle(Array.from(word)) 
+    return { 
+      word, 
+      chars, 
+      attempt: 1, 
+      guess: '', 
+      completed: false 
+    } 
+}
