0ed59aa02ffbec8347883f2cbaec433b2be7827e
mingmaomak <61099491+mingmaomak@users.noreply.github.com>
2021-07-23 14:47:54 +0700

    Update App.js to use WordCard and fixed Wordcard.js

--- App.js
+++ App.js
@@ -22,14 +22,12 @@ import './App.css';
 //     </div>
 //   );
 // }
-import CharacterCard from './CharacterCard'; 
-const word = "Hello"; 
+import CharacterCard from './CharacterCard';
+import WordCard from './WordCard'; 
 function App() { 
   return ( 
     <div> 
-      { 
-         Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>) 
-      } 
+      <WordCard value="hello"/> 
     </div> 
   ); 
 }
--- WordCard.js
+++ WordCard.js
@@ -1,4 +1,6 @@
-import React from 'react'; 
+import React from 'react';
+import CharacterCard from './CharacterCard';
+
 export default function WordCard(props){ 
     return ( 
       <div> 
